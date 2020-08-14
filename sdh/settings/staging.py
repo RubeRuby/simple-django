8e65656ef465b2a82bbd991a08609e64d5182338
RubeRuby <rubenvanderknaap@live.nl>
2020-08-14 10:38:06 +0200

    Disable auto config secret key

--- sdh/settings/staging.py
+++ sdh/settings/staging.py
@@ -1,5 +1,5 @@
-# from .base import *
+from .base import *
 
 print('secret_key before:', SECRET_KEY)
-django_heroku.settings(locals())
+django_heroku.settings(secret_key=False, locals())
 print('secret_key after:', SECRET_KEY)
